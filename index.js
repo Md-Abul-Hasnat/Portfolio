26bde0375a60bc0646722b1f3c1a8631de2c828e
Hasnat <webdevhasnat@gmail.com>
2022-11-11 08:40:59 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".fa-bars");
 const navRight = document.querySelector(".nav-right");
 const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-right > li a");
 
 menuIcon.addEventListener("click", function () {
   navRight.classList.toggle("show-nav");
@@ -9,3 +10,9 @@ menuIcon.addEventListener("click", function () {
 window.addEventListener("scroll", function () {
   nav.classList.toggle("nav-bg", window.scrollY > 0);
 });
+
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    navRight.classList.remove("show-nav");
+  })
+);
